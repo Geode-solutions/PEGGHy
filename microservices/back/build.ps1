b2324109a40f8a0a7763ae44a62a8bccff969043
SpliiT <spliitdev@gmail.com>
2025-09-17 13:46:58 +0200

    rm powershell files

--- microservices/back/build.ps1
+++
@@ -1,9 +0,0 @@
-echo "Building back microservice"
-$back_path=".\microservices\back"
-$dist_path="$back_path\dist"
-$venv_path="$back_path\venv"
-. "$venv_path\Scripts\activate"
-$site_packages_path="$venv_path\Lib\site-packages"
-pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_back --collect-data pegghy_back --recursive-copy-metadata pegghy_back "$site_packages_path\pegghy_back\app.py" --distpath $dist_path -n pegghy-back --clean
-Copy-Item $dist_path\pegghy-back.exe .\
