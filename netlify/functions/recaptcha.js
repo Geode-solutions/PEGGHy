897586a3a0af98639fed4cea4a7a73aeff86f35c
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-02-04 16:53:19 +0100

    test later return

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,9 +1,10 @@
 // import { check_recaptcha_params } from "@geode/opengeodeweb-front/app/utils/recaptcha.js"
 
 exports.handler = async function (event) {
+  const body = event.body
+  console.log("handler", { body })
+  const { name, email, launch } = JSON.parse(body)
+  console.log("handler", { name, email, launch })
   return { statusCode: 200, body: JSON.stringify({ message: "OK" }) }
-  // const body = event.body
-  // console.log("handler", { body })
-  // const { name, email, launch } = JSON.parse(body)
   // return check_recaptcha_params(name, email, launch)
 }
