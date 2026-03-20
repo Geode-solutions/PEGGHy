261a0743bb7451fa2fb0f074cf7de5337de300a0
MaxNumerique <maximilienlandois@gmail.com>
2026-03-20 12:17:23 +0100

    async

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -5,7 +5,7 @@
 import { check_recaptcha_params } from "@geode/opengeodeweb-front/app/utils/recaptcha.js"
 console.log("Module recaptcha chargé – top level log")
 
-exports.handler = async function (event) {
+exports.handler = function (event) {
   console.log("body", event.body)
   const { name, email, launch } = JSON.parse(event.body)
   console.log(name, email, launch)
