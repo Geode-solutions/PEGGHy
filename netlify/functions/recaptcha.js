49d802650107b07706e6494d38c22bf92f830b17
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-03-20 08:53:11 +0100

    fix(Netlify): parse
    Refactor recaptcha handler to log request body and return a test response.

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -5,19 +5,22 @@
 import { check_recaptcha_params } from "@geode/opengeodeweb-front/app/utils/recaptcha.js"
 console.log("Module recaptcha chargé – top level log");
 
-exports.handler = async function (event, context) {
-  console.log("Handler démarré !");
-  console.log("Event:", JSON.stringify(event));
+//exports.handler = async function (event, context) {
+  //console.log("Handler démarré !");
+  //console.log("Event:", JSON.stringify(event));
+  //return {
+    //statusCode: 200,
+    //body: JSON.stringify({ message: "Test minimal OK – pas de 502" })
+  //};
+//};
 
+exports.handler = async function (event) {
+  console.log("body", event.body)
+  const { name, email, launch } = JSON.parse(event.body)
+  console.log(name, email, launch)
+  //return check_recaptcha_params(name, email, launch)
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Test minimal OK – pas de 502" })
   };
-};
-
-//exports.handler = async function (event) {
-//  console.log("body", event.body)
-//  const { name, email, launch } = JSON.parse(event.body)
-  //console.log(name, email, launch)
-  //return check_recaptcha_params(name, email, launch)
-//}
+}
