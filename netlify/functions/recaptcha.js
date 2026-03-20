e6c56ba8637a710a079bbf60d12b6f5b3ea730a5
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-03-20 08:46:44 +0100

    Refactor recaptcha function to improve logging
    Refactor recaptcha handler to log event and return a test response.

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -2,11 +2,23 @@
 //oxlint-disable oxc/func-names
 //oxlint-disable unicorn/prefer-module
 
-import { check_recaptcha_params } from "@geode/opengeodeweb-front/app/utils/recaptcha.js"
+//import { check_recaptcha_params } from "@geode/opengeodeweb-front/app/utils/recaptcha.js"
+console.log("Module recaptcha chargé – top level log");
 
-exports.handler = async function (event) {
-  console.log("body", event.body)
-  const { name, email, launch } = JSON.parse(event.body)
-  console.log(name, email, launch)
-  return check_recaptcha_params(name, email, launch)
-}
+exports.handler = async function (event, context) {
+  console.log("Handler démarré !");
+  console.log("Event:", JSON.stringify(event));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Test minimal OK – pas de 502" })
+  };
+};
+
+
+//exports.handler = async function (event) {
+//  console.log("body", event.body)
+//  const { name, email, launch } = JSON.parse(event.body)
+  //console.log(name, email, launch)
+  //return check_recaptcha_params(name, email, launch)
+//}
