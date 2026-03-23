8fd2151a66b886c8c91c71db74a4c184926ef212
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-03-23 11:26:46 +0100

    fix(Cloud): better netlify function return

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -5,6 +5,7 @@
 import { check_recaptcha_params } from "@geode/opengeodeweb-front/app/utils/recaptcha.js";
 console.log("Module recaptcha chargé – top level log");
 
+// oxlint-disable-next-line require-await
 exports.handler = async function (event) {
   console.log("body", event.body);
   const { name, email, launch } = JSON.parse(event.body);
