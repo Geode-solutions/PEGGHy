72838f24c6b77a113f1d9d8cd50d86a58bd49410
MaxNumerique <maximilienlandois@gmail.com>
2026-03-30 11:09:34 +0200

    feat(model_components): adds color options via context menu

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -193,12 +193,15 @@ async function get_viewer_id(x, y) {
   );
 }
 
-async function handleTreeMenu({ event, itemId }) {
-  const rect = cardContainer.value.$el.getBoundingClientRect();
+async function handleTreeMenu({ event, itemId, context_type, modelId }) {
+  const rect = cardContainer.value.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const yUI = event.clientY - rect.top;
 
-  const item = await dataStore.item(itemId);
+  const meta_data =
+    context_type === "model_component"
+      ? { viewer_type: "model_component", geode_object_type: "component", modelId }
+      : await dataStore.item(itemId);
 
   menuStore.openMenu(
     itemId,
@@ -208,7 +211,7 @@ async function handleTreeMenu({ event, itemId }) {
     containerHeight.value,
     rect.top,
     rect.left,
-    item,
+    meta_data,
   );
 }
 
