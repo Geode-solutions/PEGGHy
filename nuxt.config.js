d0b6117cb9174662773d2a3569bc1bd5a68a36b8
MaxNumerique <maximilienlandois@gmail.com>
2026-08-11 14:42:55 +0200

    fix(nuxt): enable prefixComposables to resolve useLayout auto-import collision

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,9 @@ export default defineNuxtConfig({
         draggable: false,
       },
     },
-    moduleOptions: {},
+    moduleOptions: {
+      prefixComposables: true,
+    },
     vuetifyOptions: {
       labComponents: true,
       theme: {
