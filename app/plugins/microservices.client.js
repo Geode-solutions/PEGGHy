f02c8c917366124aba1bfef47c5b55f4faeb5848
Tao Schiro <spliitdev@gmail.com>
2026-09-07 15:30:16 +0200

    fix: remove local upload folder path

--- app/plugins/microservices.client.js
+++ app/plugins/microservices.client.js
@@ -2,20 +2,11 @@ import { useBackStore } from "@ogw_front/stores/back";
 import { useInfraStore } from "@ogw_front/stores/infra";
 import { useViewerStore } from "@ogw_front/stores/viewer";
 
-const UPLOAD_FOLDER_PATH = "/home/tao/Bureau/Geode/GeodeWeb-Workspaces/PEGGHy/PEGGHy-Data/";
-
 export default defineNuxtPlugin(() => {
   const infraStore = useInfraStore();
 
   // Initialize and register back microservice
   const backStore = useBackStore();
-  const defaultLaunch = backStore.launch.bind(backStore);
-  backStore.launch = function launch(args) {
-    return defaultLaunch({
-      ...args,
-      uploadFolderPath: UPLOAD_FOLDER_PATH,
-    });
-  };
   infraStore.register_microservice(backStore);
 
   // Initialize and register viewer microservice
