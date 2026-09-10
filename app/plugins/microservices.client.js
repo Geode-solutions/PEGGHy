ce42e1f5bd5f3459b5699a70a2c902416ac30bc0
Tao Schiro <spliitdev@gmail.com>
2026-09-10 11:00:08 +0200

    reput logs

--- app/plugins/microservices.client.js
+++ app/plugins/microservices.client.js
@@ -3,13 +3,21 @@ import { useInfraStore } from "@ogw_front/stores/infra";
 import { useViewerStore } from "@ogw_front/stores/viewer";
 
 export default defineNuxtPlugin(() => {
+  console.log("[PLUGIN] Initializing microservices plugin...");
+
   const infraStore = useInfraStore();
 
   // Initialize and register back microservice
+  console.log("[PLUGIN] Registering back microservice");
   const backStore = useBackStore();
   infraStore.register_microservice(backStore);
 
   // Initialize and register viewer microservice
+  console.log("[PLUGIN] Registering viewer microservice");
   const viewerStore = useViewerStore();
   infraStore.register_microservice(viewerStore);
+
+  console.log(infraStore.microservices);
+
+  console.log("[PLUGIN] All microservices registered and stores initialized");
 });
